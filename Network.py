01a3a54669836a3a5267b1a9e60e89316c68a716
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2016-03-12 15:41:40 +0200

    Task 02 commit without gui nor server

--- Network.py
+++ Network.py
@@ -14,8 +14,15 @@ def lab2_update(neuron, error, eta, x):
     neuron.weights += (eta *error* x)
     return neuron
 
+def lab3_update(neuron, error, eta, x = None):
+    neuron.weights += (eta * error)
+    return neuron
+
 def lab2_error(y_, y):
     return y-y_
+
+def LMS(x):
+    return 0.5*x*x
 
 class Perceptron:
     def __init__(self, act_func, d):
@@ -27,6 +34,13 @@ class Perceptron:
         v = np.sum(self.weights * x + self.bias)
         return self.activation_function(v)
 
+    def getWeights(self):
+        res = ""
+        for w in self.weights[0]:
+            res += str(w)
+            res += ", "
+        return res
+
 class SingleNeuronNetwork:
     def __init__(self, neuron, error_func, update_func):
         self.neuron = neuron
@@ -34,23 +48,45 @@ class SingleNeuronNetwork:
         self.update_function = update_func
 
     def train(self, train_data, test_data, eta = 0.1, epochs = 50):
-
+        res = ""
+        mse = []
         for i in xrange(epochs):
-            for x, y in train_data:
-
-                y_ = self.neuron.predict(x)
-
-                error = self.error_function(y_,y)
-                self.neuron = self.update_function(self.neuron, error, eta, x)
-            print self.neuron.weights
-            self.test(test_data)
+            error_cumulative  = 0
+            errors = []
+            for patch in train_data:
+                for x, y in patch:
+                    y_ = self.neuron.predict(x)
+                    error = self.error_function(y_, y)
+                    errors.append(error)
+                    error_cumulative += error * x
+            self.neuron = self.update_function(self.neuron, error_cumulative, eta, None)
+            mse.append(np.mean(LMS(np.array(errors))))
+            res += "Epoch#" + str(i) + ": "
+            res += self.neuron.getWeights()
+            res += "\n"
+        return res
 
     def test(self, test_data):
+        conf_mat = [[0 for i in xrange(3)]for i in xrange(3)]
         total_error = 0
         for x, y in test_data:
             y_ = self.neuron.predict(x)
+            ixy = 0
+            ixy_ = 0
+            if y_ == -1:
+                ixy_ = 0
+            elif y_ == 1:
+                ixy_ = 1
+
+            if y == -1:
+                ixy = 0
+            elif y == 1:
+                ixy = 1
+
+            conf_mat[ixy_][ixy] += 1
 
             error = self.error_function(y_,y)
-            total_error += error
+            if y_ != y:
+                total_error += 1
 
-        print total_error
+        return conf_mat, total_error
--- Task01.py
+++ Task01.py
@@ -2,14 +2,48 @@ from __future__ import division
 import Loader
 import Network
 import numpy as np
+from socket import *
+import time
 
-loader = Loader.IrisLoader("Data/Iris Data.txt", "r")
-result = loader.load()
+HOST = ''
+PORT = 29876
+ADDR = (HOST, PORT)
+BUFSIZE = 4096
+serv = socket(AF_INET, SOCK_STREAM)
 
-data = []
+serv.bind((ADDR))
+serv.listen(5)
+
+conn, addr = serv.accept()
+
+conn.send('TEST_HANDSHAKE')
 
 filter_X1 = 1
 filter_X2 = 2
+filter_C1 = 0
+filter_C2 = 1
+
+filter_C1 = int(conn.recv(BUFSIZE))
+filter_C2 = int(conn.recv(BUFSIZE))
+
+filter_X1 = int(conn.recv(BUFSIZE))
+filter_X2 = int(conn.recv(BUFSIZE))
+
+ETA = 0.1
+ETA = float(conn.recv(BUFSIZE))
+
+EPOCHS = 50
+EPOCHS = int(conn.recv(BUFSIZE))
+
+
+All_CLASSES = ["Iris-setosa",  "Iris-versicolor", "Iris-virginica"]
+
+DATA_LOC = "G:/Hell/Neural/Tasks/Data/Iris Data.txt"
+DATA_LOC = conn.recv(BUFSIZE)
+loader = Loader.IrisLoader(DATA_LOC, "r")
+result = loader.load()
+
+data = []
 
 for Class in result:
     for i in xrange(len(result[Class])):
@@ -19,8 +53,8 @@ for Class in result:
 #("Iris-setosa", -1)
 #("Iris-virginica", -1)
 #("Iris-versicolor", 1)
-Classes = [("Iris-setosa", -1),  ("Iris-versicolor", 1)]
-
+Classes = [(All_CLASSES[filter_C2], -1),  (All_CLASSES[filter_C1], 1)]
+print Classes
 
 for Class, label in Classes:
     iter_label = np.empty(len(result[Class]))
@@ -36,6 +70,20 @@ for Class in data:
     test_data.extend(Class[30:50])
 
 net = Network.SingleNeuronNetwork(Network.Perceptron(Network.step,2),Network.lab2_error,Network.lab2_update)
-net.train(train_data,test_data,0.3,50)
+log = net.train(train_data,test_data,ETA,EPOCHS)
 
-net.test(test_data)
+conf_mat, total_error = net.test(test_data)
+conf_mat_str = ""
+for i in xrange(len(conf_mat)):
+    for j in xrange(len(conf_mat[i])):
+        conf_mat_str += str(conf_mat[i][j])
+        conf_mat_str += ", "
+
+conn.send(conf_mat_str)
+gw = net.neuron.getWeights()
+conn.send(gw)
+time.sleep(1)
+conn.send(str(total_error))
+time.sleep(1)
+conn.send(log)
+conn.close()
